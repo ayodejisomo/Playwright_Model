de86ff03da0de76e39e311775e5cb6d055652237
Oluwatayo <ayodejisomo@gmail.com>
2026-03-14 18:51:59 +0100

    First commit

---
+++ tests/demo/login.spec.js
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test'
+import {LoginPage} from '../../pages/login'
+
+test('Login', async ({page}) =>{
+
+    const Login = new LoginPage(page)
+
+    await Login.gotoLoginPage()
+    await Login.login('standard_user', 'secret_sauce')
+
+    
+})
